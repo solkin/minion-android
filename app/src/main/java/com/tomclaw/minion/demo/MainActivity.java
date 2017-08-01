e173bc7fe80726a36573bf1c531b46174e32e337
Igor Solkin <inbox@tomclaw.com>
2017-08-01 22:07:02 +0300

    Начало реализации экранов демо-приложения.

--- app/src/main/java/com/tomclaw/minion/demo/MainActivity.java
+++ app/src/main/java/com/tomclaw/minion/demo/MainActivity.java
@@ -1,14 +1,7 @@
 package com.tomclaw.minion.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.tomclaw.minion.Minion;
-import com.tomclaw.minion.ResultCallback;
-import com.tomclaw.minion.storage.MemoryStorage;
-import com.tomclaw.minion.storage.StringStorage;
-import com.tomclaw.minion.storage.Writable;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,41 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ResultCallback callback = new ResultCallback() {
-
-            @Override
-            public void onReady(Minion minion) {
-
-            }
-
-            @Override
-            public void onFailure(Exception ex) {
-
-            }
-        };
-
-        Minion.lets()
-                .load(StringStorage.create("[group]\nkey=value\nkey2=value2"))
-                .async(callback);
-
-        Minion minion = Minion.lets()
-                .load(StringStorage.create("[group]\nkey=value\nkey2=value2"))
-                .sync();
-
-        Writable storage = MemoryStorage.create();
-        Minion.lets()
-                .store(storage)
-                .sync();
-        minion.setValue("settings", "lang", "ru,en");
-        minion.store();
-        Log.d("~@~", storage.toString());
-//
-//        MemoryStorage memoryStorage = MemoryStorage.create();
-//        Minion.lets()
-//                .load(memoryStorage)
-//                .and()
-//                .store(memoryStorage)
-//                .sync(callback);
     }
 }
---
+++ app/src/main/java/com/tomclaw/minion/demo/ParseActivity.java
@@ -0,0 +1,7 @@
+package com.tomclaw.minion.demo;
+
+/**
+ * Created by solkin on 01.08.17.
+ */
+public class ParseActivity {
+}
