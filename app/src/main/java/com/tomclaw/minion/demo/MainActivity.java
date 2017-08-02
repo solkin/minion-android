e22a228ec3becd312b9a9c5a9bd982094651d125
Igor Solkin <inbox@tomclaw.com>
2017-08-02 23:40:29 +0300

    Использование RecyclerView на главном экране для демонстрации списка возможностей библиотеки.

--- app/src/main/java/com/tomclaw/minion/demo/MainActivity.java
+++ app/src/main/java/com/tomclaw/minion/demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.tomclaw.minion.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
     }
 }
